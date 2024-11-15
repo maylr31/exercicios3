f91d62c8ec0a5c5e8471c175d83c29bda21b7d97
maylr31 <maylopes2122@gmail.com>
2024-11-15 02:25:04 -0300

    teste3

---
+++ teste3.py
@@ -0,0 +1,6 @@
+# Este programa exibe uma saudacao personalizada
+def saudacao ( nome ) :
+    print ("Olá, " + nome + "!")
+
+nome_usuario = input (" Digite seu nome : ")
+saudacao ( nome_usuario )
