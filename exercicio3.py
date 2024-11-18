5dc97382e0cb08f35d664433d684300baf05afb1
maylr31 <maylopes2122@gmail.com>
2024-11-18 20:05:06 -0300

    informação

--- exercicio3.py
+++ exercicio3.py
@@ -1,4 +1,5 @@
 import math
+#essa é uma função que importa a biblioteca de matematica ou a que vc desejar.
 raio = float(input('Digite o valor do raio: '))
 area = math.pi * raio**2
 print(f'A = {area:.4f}')
